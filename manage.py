c2eefbaa7cf741591e56a00cc947183b69fc99b1
Dayananda Ksh <dayanandaksh39@gmail.com>
2024-01-02 22:41:49 +0530

    added operations in manage.py
    completed most basic operations

--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
 import csv
+import os
 
 def header():
     csv_file = open("library.csv", "w")
@@ -22,6 +23,7 @@ def write_header():
 
 def add_record():
     print("Current Operation : Add Record")
+    print("-"*33)
     csv_file = open("library.csv", "a")
     csv_writer = csv.writer(csv_file)
     book_id = input("Enter Book-Id :")
@@ -33,13 +35,92 @@ def add_record():
     csv_file.close()
 
 
-def display_records():
-    csv_file = open("library.csv", "r")
+def update_record():
+    print("Current Operation : Update Record")
+    print("-"*33)
+    book_id = input("Book Id : ")
+    csv_file = open("library.csv", "r", newline="\r\n")
     csv_reader = csv.reader(csv_file)
-    print("Id=============Title============Price")
+    temp_file = open("temp.csv", "a")
+    temp_writer = csv.writer(temp_file)
     for line in csv_reader:
-        if line != []:
-            print(line[0],"\t", line[1],"\t\t", line[2])
+        if line[0] == book_id:
+            print("\nCurrent Record :")
+            print("Id".ljust(20), "Title".ljust(40), "Price")
+            print("="*80)
+            print(line[0].ljust(20), line[1].ljust(40), line[2])
+            print("\nUpdating...")
+            book_title = input("Enter new Book Title : ")
+            book_price = input("Enter new Book Price : ")
+            new_book_id = input("Enter new Book Id : ")
+            data = [new_book_id, book_title, book_price]
+            temp_writer.writerow(data)
+            print("The updated record is : ")
+            print("Id".ljust(20), "Title".ljust(40), "Price")
+            print("="*80)
+            print(data[0].ljust(20), data[1].ljust(40), data[2])
+        else:
+            temp_writer.writerow(line)
+    csv_file.close()
+    temp_file.close()
+    os.remove("library.csv")
+    os.rename("temp.csv", "library.csv")
+
+
+def delete_record():
+    print("Current Operation : Delete Record")
+    print("-"*33)
+    book_id = input("Book Id : ")
+    csv_file = open("library.csv", "r", newline="\r\n")
+    csv_reader = csv.reader(csv_file)
+    temp_file = open("temp.csv", "a")
+    temp_writer = csv.writer(temp_file)
+    for line in csv_reader:
+        if line[0] == book_id:
+            print("\nSelected Record :")
+            print("Id".ljust(20), "Title".ljust(40), "Price")
+            print("="*80)
+            print(line[0].ljust(20), line[1].ljust(40), line[2])
+            print("\nDeleting...")
+            print("Record deleted successfully.")
+        else:
+            temp_writer.writerow(line)
+    csv_file.close()
+    temp_file.close()
+    os.remove("library.csv")
+    os.rename("temp.csv", "library.csv")
+
+
+def search_record():
+    print("Current Operation : Search Record")
+    print("-"*33)
+    book_id = input("Book Id : ")
+    csv_file = open("library.csv", "r", newline="\r\n")
+    csv_reader = csv.reader(csv_file)
+    found = False
+    for line in csv_reader:
+        if line[0] == book_id:
+            print("\nFound Record :")
+            print("Id".ljust(20), "Title".ljust(40), "Price")
+            print("="*80)
+            print(line[0].ljust(20), line[1].ljust(40), line[2])
+            found = True
+    if found == False:
+        print("\nNo record found❌")
+
+
+def display_records():
+    print("Current Operation: Display Records")
+    print("-"*35)
+    csv_file = open("library.csv", "r", newline='\r\n') #newline='\r\n' removes empty line
+    csv_reader = csv.reader(csv_file)
+    next(csv_reader)
+    print("Id".ljust(20), "Title".ljust(40), "Price")
+    print("="*80)
+    for line in csv_reader:
+        print(line[0].ljust(20), line[1].ljust(40), line[2])
+
+
 def display_menu():
     print("=" * 50,)
     print("="*15, "MAIN MENU", "=" * 15)
@@ -47,11 +128,11 @@ def display_menu():
               Choose 1-5
               ----------
             1. Add Record
-            2. Modify Record
+            2. Update Record
             3. Delete Record
             4. Search Record
             5. Display All Records
-            6. Quit\n''')
+            6. Exit\n''')
 
 
 write_header()
@@ -60,8 +141,20 @@ display_menu()
 command = 0
 
 while command != 6:
-    command = int(input(">>>"))
-    if command == 1:
+    command = input("\n>>>")
+    print("\n")
+    if command == '1':
         add_record()
-    elif command == 5:
+    elif command == '2':
+        update_record()
+    elif command == '3':
+        delete_record()
+    elif command == '4':
+        search_record()
+    elif command == '5':
         display_records()
+    elif command == '6':
+        print("Library Exited Successfully.\n")
+        print("Thank You. Please  Visit Again🙏\n\n")
+    else:
+        print("Invalid Input!❌\n")
